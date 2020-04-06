f25ecd33544bc750e2c8d8837fa1cf1e89137f78
Tito Zwane <drammatik@gmail.com>
2020-04-06 17:11:32 +0100

    made routes and added to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
-
-const express = require('express')
 //It’s the server framework
-const bodyParser = require('body-parser')
+const express = require('express');
 //Responsible to get the body off of network request.
-const cors = require('cors')
+const bodyParser = require('body-parser');
 //Package for providing a Connect/Express middleware that can be used to enable CORS with various options.
-
-const db = require('./db')
+const cors = require('cors');
 //requiring the database
+const db = require('./db');
+//add router to the Server
+const movieRouter = require('./routes/movie-router');
 const app = express()
 const apiPort = 3000
 
@@ -21,5 +21,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.use('/api', movieRouter);
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
