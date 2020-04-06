26022da3868e4f18873a2bf998d8e96c8b0487a9
Tito Zwane <drammatik@gmail.com>
2020-04-06 16:40:42 +0100

    created model

---
+++ server/models/movie-model.js
@@ -0,0 +1,15 @@
+//creating schema for how my model will look like for my db
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const Movie = new Schema(
+    {
+        name: { type: String, required: true },
+        time: { type: [String], required: true },
+        rating: { type: Number, required: true },
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('movies', Movie)
